c5d56e3a7ba3b82ab154283eb1a854287c09efc5
pranav <sanaspranav28@gmail.com>
2025-06-19 19:33:04 +0530

    added clerk auth and sign in ,sign up

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { useClerk , UserButton , useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
   const isCourseListPage = location.pathname.includes('/course-list'); 
+
+  const {openSignIn} = useClerk();
+  const {user} = useUser();
+
 
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 
@@ -14,7 +19,7 @@ const Navbar = () => {
           <button>Become Educator</button>
           | <Link to='/my-enrollments'>My Enrollments</Link>
         </div>
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+        <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full cursor-pointer'>Create Account</button>
       </div>
       {/* for mobile screen */}
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
