60b8809ed21f35662c26fa7d7e208b09cfdf7b90
pranav <sanaspranav28@gmail.com>
2025-06-20 23:52:39 +0530

    updated coursecard in course section

--- src/components/student/CoursesSection.jsx
+++ src/components/student/CoursesSection.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
+import CourseCard from './CourseCard';
 
 const CoursesSection = () => {
+
+  const {allCourses} = useContext(AppContext);
+
   return (
     <div className='py-16 md:px-40 px-8'>
       <h2 className='text-3xl font-medium text-gray-800'>Powered by leading educators</h2>
-      <p className='text-sm md:text-base text-gray-500 mt-3'>Explore expertly designed courses across key domains. From technology and design to business and wellness, each course is built to drive real impact.</p>
+      <p className='text-sm md:text-base text-gray-500 mt-3'>Explore expertly designed courses across key domains.<br/>From technology and design to business and wellness, each course is built to drive real impact.</p>
       
+      <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] px-4 md:px-0 md:my-16 my-10 gap-4'>
+        {allCourses.slice(0,4).map((course,index)=> <CourseCard key={index} course={course} />)}
+      </div>
 
       <Link to={'/course-list'} onClick={()=> scrollTo(0,0)} 
       className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'>
