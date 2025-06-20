b438751c9a4505441854b50f1798567d48e30f25
pranav <sanaspranav28@gmail.com>
2025-06-20 21:32:52 +0530

    added companies and courses section in the home page

--- src/components/student/CoursesSection.jsx
+++ src/components/student/CoursesSection.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CoursesSection = () => {
   return (
-    <div>
-      <h1>Courses Section</h1>
+    <div className='py-16 md:px-40 px-8'>
+      <h2 className='text-3xl font-medium text-gray-800'>Powered by leading educators</h2>
+      <p className='text-sm md:text-base text-gray-500 mt-3'>Explore expertly designed courses across key domains. From technology and design to business and wellness, each course is built to drive real impact.</p>
+      
+
+      <Link to={'/course-list'} onClick={()=> scrollTo(0,0)} 
+      className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'>
+      Show all courses</Link>
     </div>
   )
 }
--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Hero from '../../components/student/Hero'
+import Companies from '../../components/student/Companies'
+import CoursesSection from '../../components/student/CoursesSection'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
       <Hero />
-    </div>
+      <Companies />
+      <CoursesSection />
+    </div>  
   )
 }
 
