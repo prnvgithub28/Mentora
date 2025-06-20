ac94e8d3247b126d8b0e688f9af5e52c5dba1605
pranav <sanaspranav28@gmail.com>
2025-06-21 00:24:56 +0530

    updated rating calc in coursecard using appcontext

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,9 +1,13 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useContext } from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 import { useClerk , UserButton , useUser } from '@clerk/clerk-react'
+import { AppContext } from '../../context/AppContext'
 
 const Navbar = () => {
+
+  const {navigate} = useContext(AppContext);
+
   const isCourseListPage = location.pathname.includes('/course-list'); 
 
   const {openSignIn} = useClerk();
@@ -13,7 +17,7 @@ const Navbar = () => {
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 
     ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
-      <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' ></img>
+      <img onClick={()=> navigate('/')} src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' ></img>
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
         { user && 
           <>
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,24 +1,36 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
 
     const currency = import.meta.env.VITE_CURRENCY;
+    const navigate  = useNavigate();
 
     const [allCourses,setAllCourses] = useState([]);
 
-    const fetchAllCourses = async() =>{
+    const fetchAllCourses = async() =>{//fetch all courses
         setAllCourses(dummyCourses)
     }
 
-    useEffect(() => {
+    //function to calculate average rating of course
+    const calculateRating = (course) =>{
+        if(course.courseRatings.length == 0) return 0;
+        let totalRating = 0;
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating;
+        })
+        return totalRating/course.courseRatings.length ;
+    }
+
+    useEffect(() => { 
         fetchAllCourses()
     },[])
 
     const value = {
-        currency,allCourses
+        currency,allCourses,navigate,calculateRating
     }
 
     return (
