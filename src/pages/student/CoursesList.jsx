169d337dd844154bb98b152d09a4f40ca42cdc1e
pranav <sanaspranav28@gmail.com>
2025-06-22 13:44:17 +0530

    completed left col of the player page

--- src/pages/student/CoursesList.jsx
+++ src/pages/student/CoursesList.jsx
@@ -42,7 +42,7 @@ const CoursesList = () => {
       { input && <div className='inline-flex items-center gap-4 px-4 py-2 border mt-8-mb-8 text-gray-600'>
         <p>{input}</p>
         <img src={assets.cross_icon} alt='' className='cursor-pointer' onClick={()=> navigate('/course-list')} />
-        </div>
+        </div> 
       }
       <div className='grid grid-cols-1 sm:grid-cols-2 md-grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
         {filteredCourse.map((course,index)=> <CourseCard key={index} course={course} />)}
