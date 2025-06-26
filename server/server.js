3d15179ba89bc425fbf3d766b228e11b6285af59
pranav <sanaspranav28@gmail.com>
2025-06-27 00:20:22 +0530

    Set up educator controller, routes, middlewares, and file upload config

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinary from './configs/cloudinary.js';
 
 //initialize express
 const app = express();
@@ -11,13 +14,16 @@ const app = express();
     try{
         //connect to the db
         await connectDB();
+        await connectCloudinary();
 
         //middlewares
         app.use(cors());
+        app.use(clerkMiddleware());
 
         //routes
         app.get('/', (req,res)=> res.send("API working"));
         app.post('/clerk',express.json(),clerkWebhooks);
+        app.use('/api/educator',express.json(),educatorRouter);
 
         //port
         const PORT = process.env.PORT || 5000;
