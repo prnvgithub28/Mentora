47ed58c5e9b2014600e09400d338a57a024aca00
pranav <sanaspranav28@gmail.com>
2025-06-27 22:51:47 +0530

    bug fix

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -93,7 +93,7 @@ export const stripeWebhooks = async(request,response)=>{
             courseData.enrolledStudents.push(userData);
             await courseData.save();
 
-            userData.enrolledStudents.push(courseData._id);
+            userData.enrolledCourses.push(courseData._id);
             await userData.save();
 
             purchaseData.status = 'completed';
