773e66724ba71dabde7f275ffd9445916bfc69d4
prnvgithub28 <sanaspranav28@gmail.com>
2025-08-10 19:37:20 +0530

    dashboard error fix

--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -53,7 +53,7 @@ const Dashboard = () => {
           <div className='flex items-center gap-3 shadow-card border border-blue-500 p-4 w-56 rounded-md'>
             <img src={assets.earning_icon} alt='earning_icon' />
             <div>
-              <p className='text-2xl font-medium text-gray-600'>{currency}{dashboardData.totalEarnings}</p>
+              <p className='text-2xl font-medium text-gray-600'>{currency}{dashboardData.totalEarnings.toFixed(2)}</p>
               <p className='text-base text-gray-500'>Total Earnings</p>
             </div>
           </div>
